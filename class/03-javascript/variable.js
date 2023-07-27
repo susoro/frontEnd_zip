13faa21ed6f37bf7cbf88611e311f95b871605f7
jenn <tnrud321@gmail.com>
2023-07-27 23:00:10 +0900

    test

--- class/03-javascript/variable.js
+++ class/03-javascript/variable.js
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
+=======
+// console.log('쿄쿄쿄');
+>>>>>>> Stashed changes
 let name2 
 name2 = "수경수"
 console.log(name2);
 
+<<<<<<< Updated upstream
 const age = 31
+=======
+
+const age = 31;
+>>>>>>> Stashed changes
 console.log(age);
