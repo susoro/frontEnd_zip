23f74ccbd4a984c171fd55a4cb1a2b9d7729d825
전수경(DIT221301) <sugyeongjeon@dit.co.kr>
2023-07-27 10:28:19 +0900

    배열시작

---
+++ class/03-javascript/array.js
@@ -0,0 +1,8 @@
+const array = ["배열"]
+// 배열의 길이 구하기 : array.length
+// 배열의 값 꺼내기 : array[숫자(인덱스)]
+// 배열 맨 뒤에 값 추가 : array.push()
+// 배열 맨 마지막 값 삭제 : array.pop()
+// 배열 요소 정렬 : array.sort()
+// 배열 데이터 확인 : array.includes(값)
+
