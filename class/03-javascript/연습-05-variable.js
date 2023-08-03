71630521ab7bd894ed30c48823d5ad270093b3e2
jenn <tnrud321@gmail.com>
2023-08-03 23:34:13 +0900

    Update 연습-05-variable.js

--- class/03-javascript/연습-05-variable.js
+++ class/03-javascript/연습-05-variable.js
@@ -1,1 +1,32 @@
 // 데이터 타입과 연산자
+
+
+1+1
+//2
+
+1 + "억원"
+//1억원
+
+1  + "1"
+//11
+
+"123" == 123
+//true
+
+"123" === 123
+//false
+
+true && true
+//true
+
+true && false
+//false
+
+false || true
+//true
+
+!false
+//true
+
+!true
+//false
