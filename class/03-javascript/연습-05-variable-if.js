65e3cad41683f101ffc4cf8dfa1663d51ad87003
jenn <tnrud321@gmail.com>
2023-08-04 15:41:26 +0900

    조건문 끝

--- class/03-javascript/연습-05-variable-if.js
+++ class/03-javascript/연습-05-variable-if.js
@@ -2,7 +2,30 @@ const password1 = "입력받은 비밀번호";
 const password2 = "입력받은 비밀번호 확인";
 
 if (password1 === password2){
-  alert("회원가입을 축하합니다");
+  //alert("회원가입을 축하합니다");
 } else{
-  alert("비밀번호가 다릅니다. 다시 한번 확인해 주세요");
+  //alert("비밀번호가 다릅니다. 다시 한번 확인해 주세요");
+}
+
+
+if(1+1 ===2){
+  console.log("정답입니다.")
+} else {
+  console.log("틀렸습니다.")
+}
+
+
+
+const profile = {
+  name : "수경수",
+  age : 127,
+  school : "동작"
+}
+
+if (profile.age>=20){
+  console.log("성인입니다.")
+}else if (profile.age>=8){
+  console.log("학생입니다.")
+}else if(profile.age > 0){
+  console.log("어린이 입니다.")
 }
