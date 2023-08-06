cac29987b6e01e296630f202574b6f20cbcf1176
jenn <tnrud321@gmail.com>
2023-08-06 22:44:48 +0900

    Create 연습-07-math.js

---
+++ class/03-javascript/연습-07-math.js
@@ -0,0 +1,25 @@
+
+
+//수학객체 : 자바스크립트의 수학기능을 사용하는 명령어 
+
+// 최대값 구하기
+Math.max(2,1,6)
+
+// 최소값 구하기
+Math.min(2,1,6)
+
+// 0~1 랜덤수 생성
+Math.random()
+
+//반올림
+Math.round(2.12)
+
+// 올림
+Math.ceil(2.12)
+
+// 버림
+Math.floor(2.12)
+
+
+Math.floor(Math.random()*1000000)
+String()
