691d03b1143f60ebab0c0ea9591362075681ea8b
전수경(DIT221301) <sugyeongjeon@dit.co.kr>
2023-08-08 09:40:45 +0900

    dom끝!

--- class/03-javascript/04-document.js
+++ class/03-javascript/04-document.js
@@ -1,1 +1,21 @@
 // html 파일에서 . "id값이름" 을 가진 id를 태그를 선택해서  제어한다
+
+function greeting(){
+
+    document.getElementById("target").innerText = "안녕안녕~!"
+
+    //inputTest
+    document.getElementById("inputTest").value = "하이하이"
+}
+
+
+//  textContent, innerText, innerHTML 차이점
+
+// textContent  마크업 태그를 제외한 모든 문자열을 읽고, 변경가능
+
+// innerText 는 style 과 같이 마크업 언어가 적용된 최종형태를 읽어온다. 즉 html 요소는 제거된채 render된 텍스트를 가져올떄 사용
+// 단, 최종적으로 사람이 볼수 있는 " human-readable"한 글자만 읽어올수 있다.
+
+
+// innerHTML 는 마크업 태그를 이용할수 없다
+
