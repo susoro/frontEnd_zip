2f964ab0d697ef06c15818f362358845a4beaff3
전수경(DIT221301) <sugyeongjeon@dit.co.kr>
2023-08-10 10:23:18 +0900

    다시 시도

---
+++ class/03-javascript/05-function.js
@@ -0,0 +1,13 @@
+// 1. 인증번호 생성함수를 화살표 함수로 변경 
+
+// 2. 인증번호 생성시마다 글자 색상이 바뀌도록
+// style color property
+
+
+// 자바스크립트 코드를 여기로 이동
+function auth() {
+            const token = String(Math.floor(Math.random() * 100000 )).padStart(6, "0")
+            const color1 = "#"+(Math.floor() * 0xffffff ).toString(16);
+            document.getElementById("target").innerText = token
+            token.style.color = color1
+        }
