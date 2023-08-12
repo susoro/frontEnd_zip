85c33b079c32dd9d37a11490db31be221a4947a9
sugyeongsu <tnrud321@gmail.com>
2023-08-12 23:17:08 +0900

    타이머-인증

--- class/03-javascript/timer.js
+++ class/03-javascript/timer.js
@@ -2,11 +2,24 @@
 
 
 
-setTimeoutTime(function(){
-    console.log("펑")
-},2000)
+// setTimeoutTime(function(){
+//     console.log("펑")
+// },2000)
 
 
-setInterval(function() {
-    console.log("1초가 지났다")
+// setInterval(function() {
+//     console.log("1초가 지났다")
+// },1000)
+
+// 10초 타이머
+let time = 180
+setInterval(function(){
+    if (time >= 0){
+
+        let min = Math.floor( time/60)
+        let sec = String(time % 60).padStart(2, "0") 
+        console.log(min + ":" + sec)
+        time = time -1
+    }
+    
 },1000)
