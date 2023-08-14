083a793a1a398df692eb5fc0bf552ee249abf7ca
전수경(DIT221301) <sugyeongjeon@dit.co.kr>
2023-08-14 12:57:53 +0900

    타이머 인증완료 _ 완성

--- class/03-javascript/timer.js
+++ class/03-javascript/timer.js
@@ -15,24 +15,43 @@
 //     console.log("1초가 지났다")
 // },1000)
 
+let isStarted = false;
+
 // 인증번호 날리기
 let auth = () => {
-    const token = String(Math.floor(Math.random() * 100000)).padStart(6, "0")
-    document.getElementById("target").innerText = token
 
-    let time = 180
+    if (isStarted == false) {
+        // 타이머가 작동중이 아닐때
+        isStarted = true;
+        document.getElementById("finish").disabled = false;
+        const token = String(Math.floor(Math.random() * 100000)).padStart(6, "0")
+        document.getElementById("target").innerText = token
+        document.getElementById("target").style.color = "#" + token
 
-    setInterval(function () {
+        let time = 10
+        let timer
 
-        if (time >= 0) {
-            let min = Math.floor(time / 60)
-            let sec = String(time % 60).padStart(2, "0")
-            document.getElementById("timer") .innerText = min + ":" + sec
-            time = time - 1
-        }
-    }, 1000)
+        timer = setInterval(function () {
+
+            if (time >= 0) {
+                let min = Math.floor(time / 60)
+                let sec = String(time % 60).padStart(2, "0")
+                document.getElementById("timer").innerText = min + ":" + sec
+                time = time - 1
+            } else {
+                document.getElementById("finish").disabled = true
+                isStarted = false
+                console.log("타이머 작동중")
+                clearInterval(timer)
+            }
+        }, 1000)
+
+    } else {
+        // 타이머가 작동중일때
+    }
+
 
 }
 
 
-// 테스트
+// 인증완료버튼 비활성화
