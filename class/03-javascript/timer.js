2679465f76a7b4b84b4f04d9cc4df93e20916cf2
전수경(DIT221301) <sugyeongjeon@dit.co.kr>
2023-08-14 10:33:03 +0900

    타이머 만들기

--- class/03-javascript/timer.js
+++ class/03-javascript/timer.js
@@ -1,5 +1,9 @@
-// 내장함수
-
+/*
+내장함수 : 자주 사용되는 함수를 js에서 내장하여
+편리하게 이용할 수 있도록 한것 
+시간지연함수 : setTimeout(func,time)
+시간반복함수 : setInterval(func, time)
+ */
 
 
 // setTimeoutTime(function(){
@@ -13,15 +17,15 @@
 
 // 10초 타이머
 let time = 180
-setInterval(function(){
-    if (time >= 0){
+setInterval(function () {
+    if (time >= 0) {
 
-        let min = Math.floor( time/60)
-        let sec = String(time % 60).padStart(2, "0") 
+        let min = Math.floor(time / 60)
+        let sec = String(time % 60).padStart(2, "0")
         console.log(min + ":" + sec)
-        time = time -1
+        time = time - 1
     }
-    
-},1000)
+
+}, 1000)
 
 // 테스트
